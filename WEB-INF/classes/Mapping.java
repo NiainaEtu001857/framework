0922fb3ef2f50f25162bd630e744b3ad7b7acff5
Navaloniaina <119047392+NiainaEtu001857@users.noreply.github.com>
2023-03-07 10:17:30 +0100

    Tout

---
+++ WEB-INF/classes/Mapping.java
@@ -0,0 +1,26 @@
+package etu1857.framework;
+
+/**
+ * Mapping
+ */
+public class Mapping {
+
+    String classe, methode;
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getMethode() {
+        return methode;
+    }
+
+    public void setMethode(String methode) {
+        this.methode = methode;
+    }
+
+}
